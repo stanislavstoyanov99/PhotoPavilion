66322337e15d0acfb5c920cdb3c6d095d2fdc8ac
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-05-20 00:45:39 +0300

    Add scaffolded identity pages.

---
+++ src/Web/PhotoPavilion.Web/Areas/Identity/Pages/Account/InputModels/ExternalLoginInputModel.cs
@@ -0,0 +1,34 @@
+﻿namespace PhotoPavilion.Web.Areas.Identity.Pages.Account.InputModels
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    using PhotoPavilion.Data.Common;
+    using PhotoPavilion.Data.Models.Enumerations;
+
+    public class ExternalLoginInputModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = nameof(Username))]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(DataValidation.FullNameMaxLength, ErrorMessage = "The {0} must be max {1} characters long.")]
+        [Display(Name = "Full name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [Display(Name = "Gender")]
+        public string SelectedGender { get; set; }
+
+        public IEnumerable<string> Genders { get; set; } = new[]
+        {
+            nameof(Gender.Male),
+            nameof(Gender.Female),
+        };
+    }
+}
